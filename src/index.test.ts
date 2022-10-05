142a5af9716d78c65a265d496096d43ffe328b2b
Alex Kozlov <alex.kozlov@terveystalo.com>
2022-10-05 15:53:14 +0300

    Tests fixed

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,21 +1,21 @@
 import maskFunction, { defaultMaskOptions  } from '.'
 
 const testObject = {
-  email: 'john.doe@testdomain.com',
+  email: 'john.doe@example.com',
   creditCard: '1234 5678 9000 9876',
   id: '3f8a43fd-6489-4ec7-bd55-7a1ba172d77b',
   name: 'John',
   surname: 'Doe',
   phone: '+358 40 1234567',
   subuser: {
-    email: 'john.smith@testdomain.com',
+    email: 'john.smith@example.com',
     creditCard: '0987 6543 2100 1234',
     id: '7c942511-969b-4363-af11-9667bf756733',
     name: 'John',
     surname: 'Smith',
     phone: '+358 (0)40 1234567',
     subuser: {
-      email: 'don.johnson@testdomain.com',
+      email: 'don.johnson@example.com',
       creditCard: '1234-0987-6543-0000',
       id: '12882e75-a726-4615-8631-6ee428e07592',
       name: 'Don',
